72b75a8090b0bb8be3368fbaf77c6bf12155b883
Renato Sampaio <renatocoral@gmail.com>
2016-08-25 13:51:46 -0300

    Adição da classe Servidor

---
+++ inc/servidor.hpp
@@ -0,0 +1,27 @@
+#ifndef SERVIDOR_HPP
+#define SERVIDOR_HPP
+
+#include <string>
+#include "pessoa.hpp"
+
+using namespace std;
+
+class Servidor : public Pessoa {
+private:
+   // Atributos
+	string cargo;
+   string departamento;
+   int carga_horaria;
+public:
+   // Métodos
+   Servidor(); // Método Construtor
+   ~Servidor();
+   string getCargo();
+   void setCargo(string cargo);
+   string getDepartamento();
+   void setDepartamento(string departamento);
+   int getCargaHoraria();
+   void setCargaHoraria(int carga_horaria);
+};
+
+#endif
---
+++ src/servidor.cpp
@@ -0,0 +1,37 @@
+#include "servidor.hpp"
+#include <iostream>
+
+using namespace std;
+
+Servidor::Servidor() {
+   cout << "Construtor padrão da classe Servidor" << endl;
+   setNome("");
+   setMatricula(0);
+   setCpf(0);
+   departamento = "";
+   carga_horaria = 8;
+   cargo = "";
+}
+
+Servidor::~Servidor(){ 
+   cout << "Destrutor da Classe Professor" << endl;
+}
+
+string Servidor::getCargo() {
+	return cargo;
+}
+void Servidor::setCargo(string cargo) {
+	this->cargo = cargo;
+}
+string Servidor::getDepartamento(){
+	return departamento;
+}
+void Servidor::setDepartamento(string departamento){
+	this->departamento = departamento;
+}
+void Servidor::setCargaHoraria(int carga_horaria){
+	this->carga_horaria = carga_horaria;
+}
+int Servidor::getCargaHoraria(){
+	return carga_horaria;
+}
